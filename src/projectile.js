2e582a086f1c60193f24edef748c3f5468554396
Remek Poltorak <rpoltorak@entitive.com>
2020-01-29 20:10:08 +0100

    Add and init JSXGraph library

--- src/projectile.js
+++ src/projectile.js
@@ -1,48 +1,8 @@
+import { JSXGraph } from "jsxgraph";
+
 export default function main() {
-  const canvas = document.getElementById("simulation");
-  const ctx = canvas.getContext("2d");
-  const startButton = document.getElementById("start");
-  const stopButton = document.getElementById("stop");
-
-  let animation;
-
-  const projectile = createProjectile(ctx, { x: 0, y: 0 });
-
-  function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    projectile.draw();
-
-    projectile.x += projectile.v;
-    projectile.y += projectile.v;
-
-    animation = window.requestAnimationFrame(draw);
-  }
-
-  startButton.addEventListener("click", () => {
-    animation = window.requestAnimationFrame(draw);
+  const board = JSXGraph.initBoard("simulation", {
+    boundingbox: [-5, 5, 5, -5],
+    axis: true
   });
-
-  stopButton.addEventListener("click", () => {
-    window.cancelAnimationFrame(animation);
-  });
-}
-
-function createProjectile(ctx, position) {
-  const { x, y } = position;
-
-  return {
-    x: x,
-    y: y,
-    v: 5,
-    radius: 5,
-    color: "white",
-    draw: function() {
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-      ctx.closePath();
-      ctx.fillStyle = this.color;
-      ctx.fill();
-    }
-  };
 }
