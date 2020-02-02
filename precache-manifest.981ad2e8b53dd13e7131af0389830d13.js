13ba6946321cb9123728b6d2dda488270d18f577
Remek Poltorak <rpoltorak@entitive.com>
2020-02-02 19:57:44 +0100

    Updates

---
+++ precache-manifest.981ad2e8b53dd13e7131af0389830d13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf05609349988511847af8be86c4ece",
+    "url": "/simulations/index.html"
+  },
+  {
+    "revision": "c134df9c45282d17c4ef",
+    "url": "/simulations/static/css/main.4259474c.chunk.css"
+  },
+  {
+    "revision": "8e723b6960702d20aa8c",
+    "url": "/simulations/static/js/2.02d86b11.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/simulations/static/js/2.02d86b11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c134df9c45282d17c4ef",
+    "url": "/simulations/static/js/main.324b6b41.chunk.js"
+  },
+  {
+    "revision": "9ec494e40f43c3e5a542",
+    "url": "/simulations/static/js/runtime-main.5318dbc2.js"
+  }
+]);
